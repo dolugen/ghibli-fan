9cc3e6fbeb25a9d52d26779b633f9904791c8935
Dulguun Battulga <dolugen@gmail.com>
2020-06-12 18:42:59 +0200

    tests and refactor

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ API_URL = "https://ghibliapi.herokuapp.com"
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
+class GhibliException(Exception):
+    '''generic exception for app error handling'''
+    pass
+
 
 def get_url(url, cache_time=60):
     '''caches url content for cache_time duration'''
@@ -16,6 +20,34 @@ def get_url(url, cache_time=60):
 
     return get_url_cached(url)
 
+def get_ghibli_api_resource(name, limit=250):
+    '''load a resource from the ghibli api'''
+    response = get_url(f"{API_URL}/{name}?limit={limit}")
+    if response.status_code != 200:
+        raise Exception(f'Resource unavailable! Response code: {response.status_code}')
+
+    return response.json()
+
+
+def get_movies_list():
+    return get_ghibli_api_resource('films')
+
+
+def get_people_list():
+    return get_ghibli_api_resource('people')
+
+
+def get_movies_and_people():
+    movies = get_movies_list()
+    people = get_people_list()
+
+    for movie in movies:
+        # for this movie, find its people and assign to it
+        movie_people = filter(lambda p: movie["url"] in p["films"], people)
+        movie['people'] = list(movie_people)
+    
+    return movies
+
 
 @app.route('/')
 def index():
@@ -24,14 +56,10 @@ def index():
 
 @app.route('/movies')
 def movies_list():
-    movies_response = get_url(f"{API_URL}/films?limit=250")
-    people_response = get_url(f"{API_URL}/people?limit=250")
-    all_movies = movies_response.json()
-    all_people = people_response.json()
-
-    for movie in all_movies:
-        # for this movie, find its people and assign to it
-        movie_people = filter(lambda p: movie["url"] in p["films"], all_people)
-        movie['people'] = list(movie_people)
-
-    return render_template('movies.html', entries=all_movies)
+    api_error = False
+    try:
+        movies = get_movies_and_people()
+    except:
+        movies = []
+        api_error = True
+    return render_template('movies.html', entries=movies, api_error=api_error)
--- tests.py
+++ tests.py
@@ -1,3 +1,86 @@
+from unittest.mock import Mock
+import json
+import logging
+import requests
+import pytest
+from pytest_mock import mocker
+from requests import Response
 
-def test_movies():
-    assert True
+
+from app import app,  get_movies_and_people, get_movies_list, get_people_list, get_ghibli_api_resource
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    return app.test_client()
+
+
+@pytest.fixture
+def movies_json():
+    with open('movies.json') as f:
+        yield json.loads(f.read())
+
+
+@pytest.fixture
+def people_json():
+    with open('people.json') as f:
+        yield json.loads(f.read())
+
+
+def test_movies_page(client, mocker):
+    '''
+    test that movies page is available and
+    uses get_movies_and_people function
+    '''
+    get_movies_and_people = mocker.patch('app.get_movies_and_people')
+    r = client.get('/movies')
+    assert r.status_code == 200
+    get_movies_and_people.assert_called()
+
+
+def test_get_movies_list(mocker):
+    '''test that a correct request is sent for movies'''
+    get_ghibli_api_resource = mocker.patch('app.get_ghibli_api_resource')
+    get_ghibli_api_resource.status_code.return_value = 200
+    get_movies_list()
+    get_ghibli_api_resource.assert_called_with('films')
+
+def test_get_people_list(mocker):
+    '''test that a correct request is sent for people'''
+    get_ghibli_api_resource = mocker.patch('app.get_ghibli_api_resource')
+    get_ghibli_api_resource.status_code = 200
+    get_people_list()
+    get_ghibli_api_resource.assert_called_with('people')
+
+def test_api_call_for_films(mocker):
+    '''test that api calls are correct'''
+    get_url = mocker.patch('app.get_url')
+    res = Mock(Response)
+    res.status_code = 200
+    res.json.return_value = []
+    get_url.return_value = res
+    get_ghibli_api_resource('films')
+    get_url.assert_called_with("https://ghibliapi.herokuapp.com/films?limit=250")
+
+    get_ghibli_api_resource('people')
+    get_url.assert_called_with("https://ghibliapi.herokuapp.com/people?limit=250")
+
+
+def test_get_movies_and_people(mocker, movies_json, people_json):
+    '''test that movies are correctly annotated with people'''
+    get_movies_list = mocker.patch('app.get_movies_list')
+    get_people_list = mocker.patch('app.get_people_list')
+    get_movies_list.return_value = movies_json
+    get_people_list.return_value = people_json
+
+    movies_with_people = get_movies_and_people()
+    get_movies_list.assert_called_once()
+    get_people_list.assert_called_once()
+    
+    # the first movie in the dataset
+    movie = movies_with_people[0]
+    assert movie['title'] == 'Castle in the Sky'
+    # the only person for this movie
+    person = movie['people'][0]
+    assert person['name'] == 'Colonel Muska'
+    assert person['films'] == [movie['url']]
